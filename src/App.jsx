1d0d2f59b4ce84bab1a65ce2a5f709bdf3645f40
omkar <161125388+OmkarSaicharan@users.noreply.github.com>
2026-01-29 15:25:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -547,7 +547,7 @@ const App = () => {
           <div className="portfolio-grid">
             <div className="portfolio-item">
               <img
-                src="https://pixabay.com/images/download/shopping-4694470_1920.jpg"
+                src="https://pixabay.com/get/gcc78b864dfa7d5df294d4b64df10c669bc410af0648cc2aa65d186b22860042ac7de745f7ae23ce48485a78e93cb9e0f0d7d594574bf3458da344b994f9340c0_1920.jpg"
                 alt="E-commerce Website"
                 className="portfolio-img"
               />
